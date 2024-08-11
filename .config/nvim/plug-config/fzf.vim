3297251b4649735305c9aa5adfe06ad1046d05a7
audioz <audioz1337@gmail.com>
2024-08-11 16:03:49 +0300

    rm cache

--- .config/nvim/plug-config/fzf.vim
+++
@@ -1,7 +0,0 @@
-
-
-nnoremap <leader>e :Files ..<CR>
-
-let g:fzf_preview_window = 'right:50%'
-let g:fzf_layout = { 'window': { 'width': 0.9, 'height': 0.6  }  }
-
--- .config/screenlayout/main.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --mode 1920x1080 --pos 1920x768 --rotate normal --output HDMI-0 --mode 1366x768 --pos 1260x0 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x768 --rotate normal --output HDMI-2 --off
